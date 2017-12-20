1a2864d4e8d01e04db976fe1021c3362f8b58df6
choijiwung <784wldnd@naver.com>
2017-12-20 15:22:35 +0900

    1~n까지 더하기

--- src/recursion/code01.java
+++ src/recursion/code01.java
@@ -1,5 +1,16 @@
 package recursion;
 
 public class code01 {
+	public static void main(String[] args) {
+		int result = func(4);
+		System.out.println(result);
+	}
+
+	private static int func(int n) {
+		if (n==0)
+			return 0;
+		else
+			return n + func(n-1);
+	}
 
 }
